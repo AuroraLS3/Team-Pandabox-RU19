24c9ae026f0a0ffdc538fd5dfeb5f3ca7e50fbd0
Rsl1122 <djrapitops@hotmail.com>
2019-10-12 12:14:58 +0300

    Imaging + Remote driving

--- ev3/devices/camera.py
+++ ev3/devices/camera.py
@@ -1,5 +1,25 @@
+import subprocess
+import threading
+
+
 def take_img(file_name):
     # Bash command for taking a picture with the webcam.
-    command = "fswebcam -r 640x480 --jpeg 100 -D 0 -S 30 %s" % file_name
+    # command = "fswebcam -r 640x480 --jpeg 100 -D 0 -S 30 %s" % file_name
 
-    # TODO implement call to the command
+    subprocess.run(["fswebcam", "-r", "-b", "640x480", "--no-banner",
+                    "--jpeg", "100", "-D", "0", "-S", "30", file_name])
+
+
+class Imaging(threading.Thread):
+    def __init__(self):
+        self.running = True
+        self.picNo = 1
+        threading.Thread.__init__(self)
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        while self.running:
+            take_img("img/shot_%d.jpeg" % self.picNo)
+            self.picNo += 1
--- ev3/main.py
+++ ev3/main.py
@@ -14,12 +14,16 @@ import evdev
 from devices.controller import buttons, sticks, Button, Stick, is_controller_connected
 from move.custom_tank import CustomMoveTank
 from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_D
+from devices.camera import Imaging
 
 def main():
     prepare_console()
 
     class State:
         run = True
+        picNo = 1
+        speedL = 0
+        speedR = 0
 
     tank = CustomMoveTank(OUTPUT_D, OUTPUT_A)
 
@@ -32,6 +36,17 @@ def main():
     def move(): 
         tank.move_cm(10)
 
+    def snap():
+        imaging_thread = Imaging()
+        imaging_thread.setDaemon(True)
+        imaging_thread.start()
+
+    def left(value):
+        State.speedL = value
+
+    def right(value):
+        State.speedR = value
+
     debug("Connected devices:")
     devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
     for device in devices:
@@ -41,9 +56,17 @@ def main():
     buttons.add(Button.RIGHT, turn(90))
     buttons.add(Button.LEFT, turn(-90))
     buttons.add(Button.UP, move)
+    
+    buttons.add(Button.R1, snap)
+
+    sticks.add(Stick.LEFT_Y, left)
+    sticks.add(Stick.RIGHT_Y, right)
 
     while (State.run):
-        time.sleep(1)
+        if State.speedL != 0 or State.speedR != 0:
+            tank.on(State.speedR, State.speedL)
+        else:
+            tank.off()
 
     debug("Shutting down in 10 seconds")
     # allow reading of console output
